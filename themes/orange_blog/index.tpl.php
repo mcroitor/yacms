bece549d7d8586b5725b0e8ad98ee5bbec69b378
Михаил Кройтор <m_croitor@hotmail.com>
2016-12-05 16:35:45 +0200

    Добавлено несколько бесплатных тем с сайта http://all-free-download.com/

---
+++ themes/orange_blog/index.tpl.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+        <meta encoding=UTF-8" />
+        <!-- additional_metas -->
+        <link rel="stylesheet" href="themes/defines.css" />
+        <link rel="stylesheet" href="<!-- theme_path -->/styl.css" />
+        <!-- additional_styles -->
+        <!-- additional_scripts -->
+        <title><!-- page_title --></title>
+
+</head>
+<body>
+<div id="page">
+  <h1 id="header"><a href="./"><!-- page_header --></a> </h1>
+  <div id="left">
+    <!-- page_content -->
+  </div>
+  <div id="right">
+    <!-- page_primary_menu -->
+    <form id="search" action="./">
+      <p>
+        <input type="text" value="Search..." name="string" class="input" />
+        <input type="submit" value="" class="submit" />
+      </p>
+    </form>
+    <h4>Categories</h4>
+    <!-- page_aside_content -->
+  </div>
+  <div id="best">
+    <div class="col">
+      <!-- add-column1 -->
+    </div>
+    <div class="col">
+      <!-- add-column2 -->
+    </div>
+    <div class="col">
+      <!-- add-column3 -->
+    </div>
+    <div class="cleaner"></div>
+  </div>
+  <p class="footer"><!-- page_footer --></div>
+</body>
+</html>
