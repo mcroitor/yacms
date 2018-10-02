6cbb9c7856a4ab58fe60e7dc1af5f0481d0b26c6
Михаил Кройтор <m_croitor@hotmail.com>
2018-10-02 15:39:51 +0300

    'Module manager' refactoring

--- modules/modules/templates/modules.tpl.php
+++ modules/modules/templates/modules.tpl.php
@@ -1,4 +1,10 @@
 <fieldset class="node">
     <legend class="bold mg5">Manage Modules</legend>
+    <h2>Installed Modules</h2>
     <!-- modules-list -->
+    <h2>Upload new module</h2>
+    <form method="post" action="./?q=module/install">
+        <input type="file" id="module" name="module" />
+        <input type="submit" value="Upload module" />
+    </form>
 </fieldset>
