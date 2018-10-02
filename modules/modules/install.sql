b810ca7a8130ff056fe52dd7e939f305aab387cb
Михаил Кройтор <m_croitor@hotmail.com>
2018-10-02 13:58:56 +0300

    'Module manager' extended

--- modules/modules/install.sql
+++ modules/modules/install.sql
@@ -1,3 +1,3 @@
 INSERT INTO menu_links_tbl VALUES(null, 'manage modules', './?q=modules/manage', 100, 11);
 
-INSERT INTO modules_tbl VALUES(NULL, 'Modules', '20180929');
+INSERT INTO modules_tbl VALUES(NULL, 'modules', '20180929');
--- modules/modules/modules.class.php
+++ modules/modules/modules.class.php
@@ -1,10 +1,8 @@
 <?php
 
-class users {
-
-    
+class modules {    
     // main definition
-    var $name = "users";
+    var $name = "modules";
     var $version = "20180929";
 
     function __construct() {
@@ -13,7 +11,19 @@ class users {
     function process_modules_manage(){
         Page::$modules["users"]->check_permissions(users::LEVEL_ADMINISTRATOR);
         $template = load_data(MODULE_PATH . $this->name . "/templates/modules.tpl.php");
-        $data = ["<!-- modules-list -->" => ""];
+        $data = ["<!-- modules-list -->" => "<table>"];
+        $query = "SELECT * FROM modules_tbl";
+        // installed modules
+        $data["<!-- modules-list -->"] .= "<h2>Installed Modules</h2>";
+        $data["<!-- modules-list -->"] .= "<tr><th class='width-200 left'>Module Name</th><th class='width-200 left'>Module Version</th></tr>";
+        $modules = sql_query($query);    
+        foreach ($modules as $module) {
+            $data["<!-- modules-list -->"] .= "<tr><td>{$module['module_name']}</td><td>{$module['module_version']}</td></tr>";
+        }
+        $data["<!-- modules-list -->"] .= "</table>";
+        $data["<!-- modules-list -->"] .= "<h2>Upload new module</h2>";
+        $data["<!-- modules-list -->"] .= "";
+
         Page::$data["<!-- page_content -->"] = fill_template($template, $data);
     }
     
