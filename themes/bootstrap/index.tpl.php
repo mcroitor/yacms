3931fe6e9b2f495267ba92e18e948f978ab5e6de
Михаил Кройтор <m_croitor@hotmail.com>
2019-07-24 16:38:21 +0300

    bootstrap theme

--- themes/bootstrap/index.tpl.php
+++ themes/bootstrap/index.tpl.php
@@ -7,19 +7,20 @@
         <!-- additional_metas -->
         <link rel="stylesheet" href="themes/defines.css" />
         <link rel="stylesheet" href="<!-- theme_path -->/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="<!-- theme_path -->/css/rewrite.css" />
         <!-- additional_styles -->
         <!-- additional_scripts -->
         <title><!-- page_title --></title>
     </head>
     <body>
         <div id="page-container" class="container-fluid bg-white">
-            <header id="page-header" class="jumbotron text-center"><!-- page_header --></header>
+            <header id="page-header" class="jumbotron text-center bg-2"><!-- page_header --></header>
             <menu id="page-primary-menu" class="navbar text-center"><!-- page_primary_menu --></menu>
             <div class="row content">
                 <section id="page-content" class="container col-sm-9"><!-- page_content --></section>
-                <aside id="page-aside-content" class="container col-sm-3 well bg-light"><!-- page_aside_content --></aside>
+                <aside id="page-aside-content" class="container col-sm-3 well bg-3"><!-- page_aside_content --></aside>
             </div>
-            <footer id="page-footer" class="footer bg-dark"><!-- page_footer --></footer>
+            <footer id="page-footer" class="footer bg-2"><!-- page_footer --></footer>
         </div>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
