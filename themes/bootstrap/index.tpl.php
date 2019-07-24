142bec740569da91305c440bf6b15d516645a8eb
Михаил Кройтор <m_croitor@hotmail.com>
2019-07-24 16:30:34 +0300

    bootstrap theme

--- themes/bootstrap/index.tpl.php
+++ themes/bootstrap/index.tpl.php
@@ -7,7 +7,6 @@
         <!-- additional_metas -->
         <link rel="stylesheet" href="themes/defines.css" />
         <link rel="stylesheet" href="<!-- theme_path -->/css/bootstrap.min.css" />
-        <script type="text/javascript" src="<!-- theme_path -->/js/bootstrap.min.js"></script>
         <!-- additional_styles -->
         <!-- additional_scripts -->
         <title><!-- page_title --></title>
@@ -18,9 +17,11 @@
             <menu id="page-primary-menu" class="navbar text-center"><!-- page_primary_menu --></menu>
             <div class="row content">
                 <section id="page-content" class="container col-sm-9"><!-- page_content --></section>
-                <aside id="page-aside-content" class="container col-sm-3 well bg-primary"><!-- page_aside_content --></aside>
+                <aside id="page-aside-content" class="container col-sm-3 well bg-light"><!-- page_aside_content --></aside>
             </div>
             <footer id="page-footer" class="footer bg-dark"><!-- page_footer --></footer>
         </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     </body>
 </html>
