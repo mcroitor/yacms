72ac2702baecb4d188c54d2aecacc8fef908f6ed
Михаил Кройтор <m_croitor@hotmail.com>
2019-07-24 15:54:18 +0300

    bootstrap theme

--- themes/bootstrap/index.tpl.php
+++ themes/bootstrap/index.tpl.php
@@ -6,9 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <!-- additional_metas -->
         <link rel="stylesheet" href="themes/defines.css" />
-<!--        <link rel="stylesheet" href=" theme_path /css/bootstrap.min.css" />-->
-        <link rel="stylesheet" href="css/bootstrap.min.css" />
-        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="<!-- theme_path -->/css/bootstrap.min.css" />
+        <script type="text/javascript" src="<!-- theme_path -->/js/bootstrap.min.js"></script>
         <!-- additional_styles -->
         <!-- additional_scripts -->
         <title><!-- page_title --></title>
