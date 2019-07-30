e9c120d68baed731346f356ac3e368a61c1d875e
Михаил Кройтор <m_croitor@hotmail.com>
2019-07-30 13:25:40 +0300

    module upload

--- modules/modules/templates/modules.tpl.php
+++ modules/modules/templates/modules.tpl.php
@@ -3,7 +3,8 @@
     <h2>Installed Modules</h2>
     <!-- modules-list -->
     <h2>Upload new module</h2>
-    <form method="post" action="./?q=module/install">
+    <form method="post" action="./?q=module/install" enctype="multipart/form-data">
+        <input type="hidden" name="MAX_FILE_SIZE" value="300000" />
         <input type="file" id="module" name="module" />
         <input type="submit" value="Upload module" />
     </form>
