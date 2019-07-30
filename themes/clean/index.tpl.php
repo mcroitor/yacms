99856fbace2b2a11a01040b03c85e9b1448fb5d9
Михаил Кройтор <m_croitor@hotmail.com>
2019-07-30 15:43:55 +0300

    clean theme

---
+++ themes/clean/index.tpl.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- additional_metas -->
+        <link rel="stylesheet" href="themes/defines.css" />
+        <link rel="stylesheet" href="<!-- theme_path -->/css/main.css" />
+        <!-- additional_styles -->
+        <!-- additional_scripts -->
+        <title><!-- page_title --></title>
+    </head>
+    <body>
+        <div id="page-container">
+            <header id="page-header"><!-- page_header --></header>
+            <menu id="page-primary-menu"><!-- page_primary_menu --></menu>
+                <section id="page-content"><!-- page_content --></section>
+                <aside id="page-aside-content"><!-- page_aside_content --></aside>
+            <footer id="page-footer"><!-- page_footer --></footer>
+        </div>
+    </body>
+</html>
