31f594bd14a57eb867252af9dd012600a4c17d9e
Михаил Кройтор <m_croitor@hotmail.com>
2019-07-30 16:08:36 +0300

    clean theme

--- modules/users/templates/login_form.tpl.php
+++ modules/users/templates/login_form.tpl.php
@@ -1,4 +1,4 @@
-<form method="POST" action="./?q=user/login" id="auth">
+<form method="POST" action="./?q=user/login" id="form-login">
     <input type='text' name='login' placeholder='login' />
     <input type='password' name='password' placeholder='password' />
     <input type='submit' value='>>' />
