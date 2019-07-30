2d2782851c15de67aeae98103f568cda87f4563a
Михаил Кройтор <m_croitor@hotmail.com>
2019-07-30 13:43:08 +0300

    custom site property creation

--- modules/users/templates/property_create.tpl.php
+++ modules/users/templates/property_create.tpl.php
@@ -1,13 +1,15 @@
 <fieldset class="node form-group">
     <legend class="bold mg5 label">Create custom site property</legend>
     <form method="POST" action="./?q=property/create">
-        <input type="text" name="variable_name" />
-        <input type="text" name="variable_value" />
-        <select name="variable_type">
-            <option>number</option>
-            <option selected="selected">text</option>
-            <option>enumerate</option>
-        </select>
-        <input type="submit" value="Create" />
+        <table>
+            <tr><td>variable name</td><td><input type="text" name="variable_name" class="form-check-input" /></td></tr>
+            <tr><td>variable value</td><td><input type="text" name="variable_value" class="form-check-input" /></td></tr>
+            <tr><td>variable value</td><td><select name="variable_value" class="form-check-input">
+                        <option>number</option>
+                        <option selected="selected">text</option>
+                        <option>enumerate</option>
+                    </select></td></tr>
+        </table>
+        <input type="submit" value="Create" class="form-button-submit" />
     </form>
 </fieldset>
